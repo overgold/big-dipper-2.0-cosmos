7a7f48a1f7fb1fb693aa00bd84d36970ef808441
Vladislav <olifirenkovlad@gmail.com>
2022-05-04 03:00:21 +0600

    fixed switcher style

--- src/screens/home/components/hero/components/switcher/styles.ts
+++ src/screens/home/components/hero/components/switcher/styles.ts
@@ -1,41 +1,44 @@
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 
 export const useStyles = () => {
-  const styles = makeStyles(theme => {
-    return {
-      switcher: {
-        maxWidth: '360px',
-        width: '100%',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        border: '1px solid #282828',
-        borderRadius: '5px',
-        boxSizing: 'border-box',
-        [theme.breakpoints.down('sm')]: {
-          flexWrap: 'wrap',
-        },
-      },
-      switcherItem: {
-        width: '50%',
-        padding: '7px 28px',
-        cursor: 'pointer',
-        fontWeight: '400',
-        fontSize: '20px',
-        lineHeight: '33px',
-        color: '#FFFFFF',
-        borderRadius: '5px',
-        boxSizing: 'border-box',
-        transition: 'all .3s linear',
-        [theme.breakpoints.down('sm')]: {
+  const styles = makeStyles(
+    theme => {
+      return {
+        switcher: {
+          maxWidth: '360px',
           width: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+          border: '1px solid #282828',
+          borderRadius: '5px',
+          boxSizing: 'border-box',
+          [theme.breakpoints.down('sm')]: {
+            flexWrap: 'wrap',
+          },
         },
-      },
-      switcherItemActive: {
-        background: '#282828',
-      },
-    };
-  })();
+        switcherItem: {
+          width: '50%',
+          padding: '7px 28px',
+          cursor: 'pointer',
+          fontWeigth: '400',
+          fontSize: '20px',
+          lineHeight: '33px',
+          color: '#FFFFFF',
+          borderRadius: '5px',
+          boxSizing: 'border-box',
+          transition: 'all .3s linear',
+          [theme.breakpoints.down('sm')]: {
+            width: '100%',
+          },
+        },
+        switcherItemActive: {
+          background: '#282828',
+        },
+      };
+    },
+    { index: 1 }
+  )();
 
   return {
     classes: styles,
