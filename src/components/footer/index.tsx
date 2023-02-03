2feff2bb9edb80741317c337c0e7641e04e818f6
i.polovinka <i.polovinka@vipcoingold.com>
2023-02-03 17:40:00 +0200

    feat:[VC-9834] update footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -2,13 +2,9 @@ import React from 'react';
 import classnames from 'classnames';
 import useTranslation from 'next-translate/useTranslation';
 import Overgold from '@assets/logo.svg';
-import {
-  Divider, Typography,
-} from '@material-ui/core';
+import { Divider, Typography } from '@material-ui/core';
 import { SocialMedia } from './components';
-import {
-  footerLinks,
-} from './utils';
+import { footerLinks } from './utils';
 import { useStyles } from './styles';
 
 const Footer: React.FC<{ className?: string }> = ({ className }) => {
@@ -18,47 +14,32 @@ const Footer: React.FC<{ className?: string }> = ({ className }) => {
   return (
     <div className={classnames(className, classes.root)}>
       <div className={classnames('footer')}>
-        {/* ============================= */}
-        {/* logo */}
-        {/* ============================= */}
-        <div className="footer__logo--container">
-          <Overgold
-            width="180"
-            height="37"
-            viewBox="0 0 211 37"
-            className="footer__logo"
-          />
-        </div>
-        {/* ============================= */}
-        {/* links */}
-        {/* ============================= */}
         <div className="footer__links">
-          {footerLinks.map((group) => {
+          <div className="footer__logo--container">
+            <Overgold
+              width="180"
+              height="37"
+              viewBox="0 0 211 37"
+              className="footer__logo"
+            />
+          </div>
+          {footerLinks.map(group => {
             return (
               <div key={group.key} className={`${group.key} links__group`}>
-                <h3>{t(`common:${group.key}`)}</h3>
-                {group.links.map((x) => {
+                {/* <h3>{t(`common:${group.key}`)}</h3> */}
+                {group.links.map(x => {
                   return (
-                    <a
-                      key={x.url}
-                      href={x.url}
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      {t(`common:${x.key}`)}
-                    </a>
+                    // <div key={x.url} className={`${group.key} link`}>
+                      <a key={x.url}  href={x.url} target="_blank" rel="noreferrer">
+                        {t(`common:${x.key}`)}
+                      </a>
+                    // </div>
                   );
                 })}
               </div>
             );
           })}
-          {/* ============================= */}
-          {/* social */}
-          {/* ============================= */}
-          <div className="footer__social">
-            <h3>{t('common:community')}</h3>
-            <SocialMedia />
-          </div>
+         
         </div>
       </div>
       <Divider />
