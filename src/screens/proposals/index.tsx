536f55a417bc6bab5f29eca7b4a3ac1d14e249e7
i.polovinka <i.polovinka@vipcoingold.com>
2023-02-08 16:09:53 +0200

    feat:[VC-9991] add Ufo in propozsls

--- src/screens/proposals/index.tsx
+++ src/screens/proposals/index.tsx
@@ -1,22 +1,17 @@
 import React from 'react';
 import useTranslation from 'next-translate/useTranslation';
 import { NextSeo } from 'next-seo';
-import {
-  Layout,
-} from '@components';
+import { Layout } from '@components';
 import { useStyles } from './styles';
 import { List } from './components';
 import { useProposals } from './hooks';
+import UFO from '@assets/ufo.svg';
+import isEmpty from 'lodash-es/isEmpty';
 
 const Proposals = () => {
   const { t } = useTranslation('proposals');
   const classes = useStyles();
-  const {
-    state,
-    loadMoreItems,
-    itemCount,
-    isItemLoaded,
-  } = useProposals();
+  const { state, loadMoreItems, itemCount, isItemLoaded } = useProposals();
 
   return (
     <>
@@ -26,17 +21,20 @@ const Proposals = () => {
           title: t('proposals'),
         }}
       />
-      <Layout
-        navTitle={t('proposals')}
-        className={classes.root}
-      >
-        <List
-          items={state.items}
-          rawDataTotal={state.rawDataTotal}
-          isItemLoaded={isItemLoaded}
-          itemCount={itemCount}
-          loadMoreItems={loadMoreItems}
-        />
+      <Layout navTitle={t('proposals')} className={classes.root}>
+        {!isEmpty(state.items) ? (
+          <List
+            items={state.items}
+            rawDataTotal={state.rawDataTotal}
+            isItemLoaded={isItemLoaded}
+            itemCount={itemCount}
+            loadMoreItems={loadMoreItems}
+          />
+        ) : (
+          <div className="wrapper-icon">
+            <UFO />
+          </div>
+        )}
       </Layout>
     </>
   );
