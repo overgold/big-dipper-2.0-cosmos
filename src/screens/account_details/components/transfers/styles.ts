8be15a3d119a9b54d1f8e0d27c0b921a32ec5aa1
i.polovinka <i.polovinka@vipcoingold.com>
2023-04-17 14:33:10 +0300

    feat:[VC-10458]

--- src/screens/account_details/components/transfers/styles.ts
+++ src/screens/account_details/components/transfers/styles.ts
@@ -1,30 +1,28 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 export const useStyles = () => {
-  const styles = makeStyles(
-    (theme) => {
-      return ({
-        root: {
-          '& .MuiTypography-h2': {
-            marginBottom: theme.spacing(2),
-          },
+  const styles = makeStyles(theme => {
+    return {
+      root: {
+        '& .MuiTypography-h2': {
+          marginBottom: theme.spacing(2),
         },
-        wrapperTab: {
-          paddingBottom:'10px',
-          [theme.breakpoints.up('lg')]: {
-            paddingBottom:'20px',
-          },
+      },
+      wrapperTab: {
+        paddingBottom: '10px',
+        [theme.breakpoints.up('lg')]: {
+          paddingBottom: '20px',
         },
-        list: {
-          minHeight: '500px',
+      },
+      list: {
+        minHeight: '500px',
+        height: '30vh',
+        [theme.breakpoints.up('lg')]: {
           height: '30vh',
-          [theme.breakpoints.up('lg')]: {
-            minHeight: '40vh',
-          },
         },
-      });
-    },
-  )();
+      },
+    };
+  })();
 
   return styles;
 };
