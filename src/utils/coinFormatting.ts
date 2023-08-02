e933e655eb8dc8ff562e518e9fc5e502e5336fe5
i.polovinka <i.polovinka@vipcoingold.com>
2023-08-02 18:07:37 +0300

    [VC-12839] update amount

--- src/utils/coinFormatting.ts
+++ src/utils/coinFormatting.ts
@@ -1,15 +1,21 @@
+import { Decimal } from 'decimal.js';
+
 export const SATOSHI_AMOUNT = 100000000;
 
-export const convertCoinToSatoshi = (num: number): number => {
-  return num / SATOSHI_AMOUNT;
+export const convertCoinFromSatoshi = (num: Decimal.Value) => {
+  return Decimal.div(num, SATOSHI_AMOUNT).toNumber();
 };
 
-export const roundToFixed = (num: number, scale: number): string => {
-  return (convertCoinToSatoshi(num) || 0).toFixed(scale);
+export const convertCoinToSatoshi = (num: Decimal.Value) => {
+  return Decimal.mul(num, SATOSHI_AMOUNT).toNumber();
+};
+
+export const roundToFixed = (num: Decimal.Value, scale: number) => {
+  return (convertCoinFromSatoshi(num) || 0).toFixed(scale);
 };
 
 export const roundToScaleNumber = (num: number, scale: number): number => {
-  const satoshiNum = convertCoinToSatoshi(num) || 0;
+  const satoshiNum = convertCoinFromSatoshi(num) || 0;
 
   if (!('' + satoshiNum).includes('e')) {
     return +(Math.round(+`${satoshiNum}e${scale}`) + `e-${scale}`);
